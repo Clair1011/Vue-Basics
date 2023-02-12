0266a2de743697ce759faaa37f072a2dff7ce4da
Claire <cweiyu1011@gmail.com>
2023-02-12 15:14:40 +0800

    Vue basics

---
+++ app.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  computed: {
+    fullNmae(){
+      if (this.name ==='') {
+      return '';
+    }
+      return this.name + ' ' + 'Chen';
+    }
+  },
+  methods: {
+    // 將名字reset
+    resetInput(){
+      this.name = '';
+    },
+    // 設定名字
+    setName(event) {
+      this.name = event.target.value;
+    },
+    // counter + 10
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    // counter + 5
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    }
+  }
+});
+
+app.mount('#events');
